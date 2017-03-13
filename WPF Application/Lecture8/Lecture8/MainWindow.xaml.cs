58b93eedfc9041286747463411d6133faf15a040
Zach Jagoda <jagod101@mail.chapman.edu>
2017-03-12 22:28:40 -0800

    Updating Homeworks/Class Examples

---
+++ WPF Application/Lecture8/Lecture8/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (checkBoxJM.IsChecked.Value == true)
+            {
+                MessageBox.Show("Correct");
+            }
+            else if (checkBoxTJ.IsChecked.Value == true)
+            {
+                MessageBox.Show("Incorrect");
+            }
+            else if (checkBoxDT.IsChecked.Value == true)
+            {
+                MessageBox.Show("Don't believe the Fake News");
+            }
+
+        }
+
+        private void checkBoxJM_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
