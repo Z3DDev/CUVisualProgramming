bd9cb60e16c851939abbb9af019373c70cf7e5bf
Zach Jagoda <jagod101@mail.chapman.edu>
2017-03-15 09:10:52 -0800

    Practice for Quiz 2

---
+++ Practice Quiz 2/Console3/Console3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float final = 0;
+
+            Console.WriteLine("Please Enter a Number or 'quit'");
+
+            while (Console.ReadLine() != "quit")
+            {
+                string input = Console.ReadLine();
+                float number = float.Parse(input);
+                final += number;
+                Console.WriteLine("Please Enter a Number or 'quit'");
+            }
+            else
+            {
+                Console.WriteLine(final);
+            }
+
+        }
+    }
+}
