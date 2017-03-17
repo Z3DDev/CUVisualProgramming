f0053ad062aae1268d96d5761731f8220571eb98
Zach Jagoda <jagod101@mail.chapman.edu>
2017-03-17 11:46:49 -0700

    In Class Work

---
+++ objectOriented/carExample/carExample/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carExample
+{
+    class Car
+    {
+        string color;
+        string name;
+        int numDoors;
+        int numWheels;
+        int topSpeed;
+
+        public Car(string color, string name, int numDoors, int numWheels, int topSpeed)
+        {
+            this.color = color;
+            this.name = name;
+            this.numDoors = numDoors;
+            this.numWheels = numWheels;
+            this.topSpeed = topSpeed;
+        }
+
+        public Car()
+        {
+
+        }
+
+        public void printString()
+        {
+            Console.WriteLine("Your car color is " + color + " the number of doors is " + numDoors + " the number of wheels is " + numWheels + " the top speed is " + topSpeed + " and it's name is " + name);
+        }
+
+        public override string ToString()
+        {
+            return "Your car color is " + color + " the number of doors is " + numDoors + " the number of wheels is " + numWheels + " the top speed is " + topSpeed + " and it's name is " + name;
+
+        }
+    }
+}
---
+++ objectOriented/carExample/carExample/CarTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carExample
+{
+    class CarTest
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            Car c1 = new Car("purple", "yo", 36, 1, 300);
+            Car c2 = new Car("pink", "six", 5, 5, 5);
+
+            c.printString();
+            c1.printString();
+            c2.printString();
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c1.ToString());
+            Console.WriteLine(c2.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
