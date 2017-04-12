e671df3c9c4ad87663bd4b045847d97b3c374fb6
Zach Jagoda <jagod101@mail.chapman.edu>
2017-04-12 11:48:46 -0700

    Fixed Folders and more Content

---
+++ Websites/Website2/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Website2.Startup))]
+namespace Website2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
